aab597460765b29c4342b608ea7441ad52d2a9ac
neresdev <neresdev@gmail.com>
2025-06-08 13:10:25 -0300

    producer service for Kafka messages

--- src/main/java/com/nrs/notifier/service/KafkaMessageProducerService.java
+++ src/main/java/com/nrs/notifier/service/KafkaMessageProducerService.java
@@ -1,4 +1,19 @@
 package com.nrs.notifier.service;
 
+import com.nrs.notifier.dto.NotificationDto;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
 public class KafkaMessageProducerService {
+
+    private final KafkaTemplate<String, NotificationDto> kafkaTemplate;
+
+    public KafkaMessageProducerService(KafkaTemplate<String, NotificationDto> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(final String topic, final NotificationDto message) {
+        kafkaTemplate.send(topic, message);
+    }
 }

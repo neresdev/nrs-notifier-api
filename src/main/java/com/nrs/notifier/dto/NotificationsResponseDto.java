e8ad46b6bfb21d6e3701e2904f5dd38fef2b40dc
neresdev <neresdev@gmail.com>
2025-06-08 13:10:25 -0300

    Response for send-message endpoint

--- src/main/java/com/nrs/notifier/dto/NotificationsResponseDto.java
+++ src/main/java/com/nrs/notifier/dto/NotificationsResponseDto.java
@@ -1,4 +1,11 @@
 package com.nrs.notifier.dto;
 
 public class NotificationsResponseDto {
+    private boolean success;
+    private String responseMessage;
+
+    public NotificationsResponseDto(boolean success, String responseMessage) {
+        this.success = success;
+        this.responseMessage = responseMessage;
+    }
 }

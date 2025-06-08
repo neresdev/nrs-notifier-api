ca7deefc709694db9f994ecad5d5129f4518d0bf
neresdev <neresdev@gmail.com>
2025-06-08 13:10:25 -0300

    Notifications service for handling business logic

--- src/main/java/com/nrs/notifier/service/NotificationsService.java
+++ src/main/java/com/nrs/notifier/service/NotificationsService.java
@@ -1,4 +1,26 @@
 package com.nrs.notifier.service;
 
+import com.nrs.notifier.dto.NotificationDto;
+import com.nrs.notifier.dto.NotificationsResponseDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
 public class NotificationsService {
+
+    @Value("${notifications.kafka.topic}")
+    private String notificationsKafkaTopic;
+
+    private final KafkaMessageProducerService kafkaMessageProducerService;
+
+    public NotificationsService(KafkaMessageProducerService kafkaMessageProducerService) {
+        this.kafkaMessageProducerService = kafkaMessageProducerService;
+    }
+
+    public NotificationsResponseDto sendNotification(final NotificationDto notificationDto) {
+        kafkaMessageProducerService.sendMessage(notificationsKafkaTopic, notificationDto);
+        return new NotificationsResponseDto(true, "Success message sent: timestamp: " + LocalDateTime.now());
+    }
 }

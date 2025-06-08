941ea509066772daf164c288b635e32e47305fda
neresdev <neresdev@gmail.com>
2025-06-08 11:59:07 -0300

    first commit - adding project to repo

---
+++ src/main/java/com/nrs/notifier/NotifierApiApplication.java
@@ -0,0 +1,13 @@
+package com.nrs.notifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifierApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifierApiApplication.class, args);
+	}
+
+}

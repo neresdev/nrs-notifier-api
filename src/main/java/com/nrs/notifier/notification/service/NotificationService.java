e313cfa44e7401a220ceb205fcadabb88bb2b463
neresdev <neresdev@gmail.com>
2025-06-17 12:59:43 -0300

    removing KafkaTemplate.send and using SendNotificationFactory

--- src/main/java/com/nrs/notifier/notification/service/NotificationService.java
+++ src/main/java/com/nrs/notifier/notification/service/NotificationService.java
@@ -2,23 +2,26 @@ package com.nrs.notifier.notification.service;
 
 import com.nrs.notifier.dto.NotificationMessage;
 import com.nrs.notifier.dto.NotificationResponseDto;
+import com.nrs.notifier.notification.service.enm.NotificationType;
 import com.nrs.notifier.service.KafkaMessageProducerService;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
 @Service
-public class NotificationServiceawd {
+public class NotificationService {
 
+    private final SendNotificationFactory factory;
 
-    private final KafkaMessageProducerService kafkaMessageProducerService;
-
-    public NotificationServiceawd(KafkaMessageProducerService kafkaMessageProducerService) {
-        this.kafkaMessageProducerService = kafkaMessageProducerService;
+    public NotificationService(SendNotificationFactory factory) {
+        this.factory = factory;
     }
 
     public NotificationResponseDto sendNotification(final NotificationMessage notificationMessage) {
-        
+        final var notificationType = NotificationType.fromString(notificationMessage.getType());
+        final var notificationService = factory.createNotificationService(notificationType);
+        notificationService.send(notificationMessage);
+
         return new NotificationResponseDto(true, "Success message sent: timestamp: " + LocalDateTime.now());
     }
 }

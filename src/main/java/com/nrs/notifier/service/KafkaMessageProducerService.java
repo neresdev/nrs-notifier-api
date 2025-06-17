fb3c2441da90e87fcfb54241090c2562fb6d908d
neresdev <neresdev@gmail.com>
2025-06-17 12:59:43 -0300

    adding topic partition key and partition number when sending messages

--- src/main/java/com/nrs/notifier/service/KafkaMessageProducerService.java
+++ src/main/java/com/nrs/notifier/service/KafkaMessageProducerService.java
@@ -1,19 +1,21 @@
 package com.nrs.notifier.service;
 
-import com.nrs.notifier.dto.NotificationDto;
+import com.nrs.notifier.dto.NotificationMessage;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 @Component
 public class KafkaMessageProducerService {
 
-    private final KafkaTemplate<String, NotificationDto> kafkaTemplate;
+    private final KafkaTemplate<String, NotificationMessage> kafkaTemplate;
 
-    public KafkaMessageProducerService(KafkaTemplate<String, NotificationDto> kafkaTemplate) {
+    public KafkaMessageProducerService(KafkaTemplate<String, NotificationMessage> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
-    public void sendMessage(final String topic, final NotificationDto message) {
-        kafkaTemplate.send(topic, message);
+    public void sendMessage(final String topic, final int partition, final NotificationMessage message) {
+        kafkaTemplate.send(topic, partition, UUID.randomUUID().toString(), message);
     }
 }

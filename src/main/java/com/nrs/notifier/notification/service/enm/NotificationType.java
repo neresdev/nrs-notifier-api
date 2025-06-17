da4a509f98e5feb1e53dc4df80e0b071b665f8be
neresdev <neresdev@gmail.com>
2025-06-17 12:59:43 -0300

    enum to define notification types

--- src/main/java/com/nrs/notifier/notification/service/enm/NotificationType.java
+++ src/main/java/com/nrs/notifier/notification/service/enm/NotificationType.java
@@ -1,4 +1,33 @@
 package com.nrs.notifier.notification.service.enm;
 
-public class NotificationType {
+import java.util.Arrays;
+
+public enum NotificationType {
+    EMAIL(0, "Email"),
+    MESSAGE(1, "SMS message"),
+    WHATSAPP(2, "Whatsapp message");
+
+    private int id;
+    private String description;
+
+    NotificationType(int id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public static NotificationType fromString(final String notificationType) {
+        return Arrays.stream(NotificationType.values())
+                .filter(t -> t == NotificationType.valueOf(notificationType))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Notification Type not implemented yet: " + notificationType));
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
